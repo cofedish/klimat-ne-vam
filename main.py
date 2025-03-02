baa6e86182889f95fc3af09ad753707f03340ae6
cofedish <COFEDISH@gmail.com>
2025-03-03 04:52:47 +0700

    Add CORS 2

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Импортируем CORS
 
 app = Flask(__name__)
-CORS(app)  # Включаем CORS для всех роутов
+CORS(app, resources={r"/*": {"origins": "*"}})  # Разрешаем запросы с любых источников
 
 # URL бота (где он будет слушать новые заявки)
-TELEGRAM_BOT_URL = 'http://bot:8374/new_request'  # Поменяй на правильный URL (если бот работает локально, то localhost)
+TELEGRAM_BOT_URL = 'http://bot:8374/new_request'
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -27,7 +27,6 @@ def callback():
         'type_of_service': type_of_service
     }
 
-    # Отправляем заявку на Telegram-бота
     response = requests.post(TELEGRAM_BOT_URL, json=payload)
 
     if response.status_code == 200:
